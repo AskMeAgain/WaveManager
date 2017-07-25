1852696c8cee7b5b142fbd10078d8bd866ec5f7a
Kelvin Petry <20632124+AskMeAgain@users.noreply.github.com>
2017-07-25 22:59:42 +0200

    Add files via upload

---
+++ Wave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave {
+
+    public Vector3 pos;
+    public int maxDistance;
+    public float scale;
+
+    public enum WaveType {
+        GrenadeStrong, GrenadeMiddle, GrenadeLow
+    };
+
+    public Wave(WaveType type) {
+
+        if (type == WaveType.GrenadeStrong) {
+            this.scale = 1f;
+            this.maxDistance = 14;
+        } else if (type == WaveType.GrenadeMiddle) {
+            this.scale = 0.6f;
+            this.maxDistance = 10;
+        } else if (type == WaveType.GrenadeLow) {
+            this.scale = 0.3f;
+            this.maxDistance = 7;
+        }
+    }
+
+}
